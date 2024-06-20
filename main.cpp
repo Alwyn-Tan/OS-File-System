6c92d83b9ddeffc6bdb33d4ead163a79e5d6c8e9
Alwyn-Tan <88386185+Alwyn-Tan@users.noreply.github.com>
2024-06-20 17:48:56 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include"block.h"
-#include <string.h>
+#include<iostream>
 #pragma warning( disable : 4996)
 using namespace std;
 
@@ -7,11 +7,13 @@ using namespace std;
 int main() {
 	format();
 	createRootDirectory();
-	char* content = new char[1024];
-	strcpy(content, "this is file1");
-	checkAndCreateFile("/root/dir1/file1", 1024, content);
-	char* content1 = new char[1024];
-	strcpy(content1, "this is file2");
-	checkAndCreateFile("/root/dir1/dir2/file2", 1024, content1);
+	// checkAndCreateFile("/root/dir1/file1", 1024);
+	// checkAndCreateFile("/root/dir1/dir2/file2", 1024);
+	bool flag = true;
+	while (flag)
+	{
+		cmdF(flag);
+	};
 
+	return 0;
 }
